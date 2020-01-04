03f9335fee7112810f04f93eb8fc3d9f7bc071cd
Travis Vela <jtravisvela@gmail.com>
2020-01-03 18:08:05 -0600

    small edit to email service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   contactAPI: 'http://guitardojo-contactservice-prod.us-east-2.elasticbeanstalk.com/',
-  emailAPI: ''
+  emailAPI: 'http://guitardojo-emailservice-prod.us-east-2.elasticbeanstalk.com/sendMail'
 };
