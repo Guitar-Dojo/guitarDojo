1ac29606396db3a810884ce20032a4fac4471b14
Travis Vela <jtravisvela@gmail.com>
2020-02-04 14:09:33 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  contactAPI: 'https://guitardojo-api-prod.us-east-2.elasticbeanstalk.com/',
-  emailAPI: 'https://guitardojo-api-prod.us-east-2.elasticbeanstalk.com/',
-  eventBriteAPI: 'https://guitardojo-api-prod.us-east-2.elasticbeanstalk.com/'
+  contactAPI: 'http://guitardojo-api-prod.us-east-2.elasticbeanstalk.com/',
+  emailAPI: 'http://guitardojo-api-prod.us-east-2.elasticbeanstalk.com/',
+  eventBriteAPI: 'http://guitardojo-api-prod.us-east-2.elasticbeanstalk.com/'
 };
