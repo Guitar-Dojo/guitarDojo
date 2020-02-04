05b898bdbcb5034765a845061aa21e617e372721
Travis Vela <jtravisvela@gmail.com>
2020-02-04 17:44:23 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  contactAPI: 'https://api.guitardojosatx.com/',
-  emailAPI: 'https://api.guitardojosatx.com/',
-  eventBriteAPI: 'https://api.guitardojosatx.com/'
+  contactAPI: 'http://api.guitardojosatx.com/',
+  emailAPI: 'http://api.guitardojosatx.com/',
+  eventBriteAPI: 'http://api.guitardojosatx.com/'
 };
